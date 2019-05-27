c159b9c506602a416e049c869096de075cac3ec0
handless <midas1024@yandex.ru>
2019-05-28 02:14:31 +0300

    Codestyle fixed

--- html_parser.py
+++ html_parser.py
@@ -4,29 +4,37 @@ TODO
 from bs4 import BeautifulSoup
 import requests
 
-#Getting the html
+URL = 'https://xn--90agdcm3aczs9j.xn--80acgfbsl1azdqr.xn--p1ai/#tab2'
+
+
+# Getting the html
 def get_html_tree():
-	page = requests.get('https://xn--90agdcm3aczs9j.xn--80acgfbsl1azdqr.xn--p1ai/#tab2')
-	soup = BeautifulSoup(page.text, 'html.parser')
-	return soup
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup
 
-#Getting discussions links
+
+# Getting discussions links
 def get_discussions():
-	discussions = set()
-	for table in get_html_tree().find_all(attrs={'class' : 'ed-content js-tab2'}):
-		discuss_links = table.find_all('a')
-		for link in discuss_links:
-			discussions.add(link.attrs['href'])
-	return discussions
+    discussions = set()
+    for table in get_html_tree().find_all(
+            attrs={'class': 'ed-content js-tab2'}):
+        discuss_links = table.find_all('a')
+        for link in discuss_links:
+            discussions.add(link.attrs['href'])
+    return discussions
 
-#Checking if there are any new discussions
+
+# Checking if there are any new discussions
 def get_new_discussions():
-	new_discussions = set()
-	with open('saved/discussions.txt', 'r') as inFile:
-		new_discussions = get_discussions().difference(set(map(str.rstrip, inFile)))
-	return new_discussions
+    new_discussions = set()
+    with open('saved/discussions.txt', 'r') as inFile:
+        new_discussions = get_discussions().difference(
+            set(map(str.rstrip, inFile)))
+    return new_discussions
 
-#Saving in out file
+
+# Saving in out file
 def save_discussions():
-	with open('saved/discussions.txt', 'w') as outFile:	
-		print('\n'.join(get_discussions()), file=outFile)
+    with open('saved/discussions.txt', 'w') as outFile:
+        print('\n'.join(get_discussions()), file=outFile)
