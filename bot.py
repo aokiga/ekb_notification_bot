0882d965bee477e269fc3dd692b49706008555ff
Gleb Oborin <oborin.gleb@yandex.ru>
2019-05-28 01:36:16 +0300

    bot posts to channel now

--- bot.py
+++ bot.py
@@ -7,12 +7,12 @@ telebot.apihelper.proxy = {'https':os.getenv("PROXY")}
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello world!")
+    bot.send_message(chat_id="@aokigahara_bot_meme", text="Hello world!")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    bot.send_message(chat_id="@aokigahara_bot_meme", text=message.text)
 
 
 bot.polling()
