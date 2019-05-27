dc0fe5ca5672bfb316132612edba92ea09ad86cc
Gleb Oborin <oborin.gleb@yandex.ru>
2019-05-28 01:48:36 +0300

    slightly fixed codestyle

--- bot.py
+++ bot.py
@@ -7,12 +7,12 @@ telebot.apihelper.proxy = {'https':os.getenv("PROXY")}
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.send_message(chat_id="@aokigahara_bot_meme", text="Hello world!")
+    bot.send_message(chat_id=os.getenv("BOT_NAME"), text="Hello world!")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.send_message(chat_id="@aokigahara_bot_meme", text=message.text)
+    bot.send_message(chat_id=os.getenv("BOT_NAME"), text=message.text)
 
 
 bot.polling()
