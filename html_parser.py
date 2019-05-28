5a38b01abf4526b4b602d374e1c22fddea959a4f
handless <midas1024@yandex.ru>
2019-05-28 19:03:13 +0300

    Modified discussions output

--- html_parser.py
+++ html_parser.py
@@ -29,13 +29,12 @@ def get_discussions():
 def get_new_discussions():
     new_discussions = set()
     with open('discussions.txt', 'r') as inFile:
-        new_discussions = get_discussions().difference(
-            set(map(str.rstrip, inFile)))
+        prev_discussions = eval(inFile.read())
+    new_discussions = get_discussions().difference(prev_discussions)
     return new_discussions
 
 
 # Saving in out file
 def save_discussions():
     with open('discussions.txt', 'w') as outFile:
-        print('\n'.join(map(lambda a: ' '.join(a),
-              get_discussions())), file=outFile)
+        print(repr(get_discussions()), file=outFile)
