67e74e5602f67ee64b9c8b108f586bfb283d533c
handless <midas1024@yandex.ru>
2019-05-28 17:59:04 +0300

    Added intros

--- bot.py
+++ bot.py
@@ -7,6 +7,9 @@ bot = telebot.TeleBot(os.getenv("TOKEN"))
 telebot.apihelper.proxy = {'https': os.getenv("PROXY")}
 
 
+intros = ["Добрый вечер. Список новых объявлений за сегодня:\n", "", "", "", ""]
+
+
 def run():
     new_discussions = html_parser.get_new_discussions()
     html_parser.save_discussions()
