bb0371a083f3116092b75c10cb5696f24bb9f6df
Gleb Oborin <oborin.gleb@yandex.ru>
2019-05-28 20:45:13 +0300

    fixed codestyle

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import random
 import telebot
 import html_parser
 
@@ -8,29 +7,29 @@ bot = telebot.TeleBot(os.getenv("TOKEN"))
 telebot.apihelper.proxy = {'https': os.getenv("PROXY")}
 SITE_URL = "обсуждения.екатеринбург.рф"
 
-intros_new = ["""Добрый вечер.
+intro_new = """Добрый вечер.
 Список новых обсуждений за сегодня:
-"""]
+"""
 
-intros_all = ["""Добрый вечер.
+intro_all = """Добрый вечер.
 Напоминаем, что в данный момент доступны следущие обсуждения:
-"""]
+"""
 
 
 def send_discussions(getter, greeter):
     discussions = getter()
     if discussions:
-        message_text = greeter[random.randint(0, len(greeter) - 1)] + '\n' + \
+        message_text = greeter + '\n' + \
             '\n\n'.join(map('\n{}'.format(SITE_URL).join, discussions))
         bot.send_message(chat_id=os.getenv("CHAT_ID"), text=message_text)
 
 
 def send_all_discussions():
-    send_discussions(html_parser.get_discussions, intros_all)
+    send_discussions(html_parser.get_discussions, intro_all)
 
 
 def send_new_discussions():
-    send_discussions(html_parser.get_new_discussions, intros_new)
+    send_discussions(html_parser.get_new_discussions, intro_new)
 
 
 if __name__ == "__main__":
