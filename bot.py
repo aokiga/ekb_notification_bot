72a530657794a175b85480b3669bfea4a345603e
handless <midas1024@yandex.ru>
2019-05-28 17:55:09 +0300

    Changed bot, so that it only activates once

--- bot.py
+++ bot.py
@@ -6,17 +6,13 @@ import html_parser
 bot = telebot.TeleBot(os.getenv("TOKEN"))
 telebot.apihelper.proxy = {'https': os.getenv("PROXY")}
 
-SLEEPING_TIME = 5
-
 
 def run():
-    while (True):
-        new_discussions = html_parser.get_new_discussions()
-        html_parser.save_discussions()
-        if new_discussions:
-            bot.send_message(chat_id=os.getenv("CHAT_ID"),
-                             text=str(new_discussions))
-        time.sleep(SLEEPING_TIME)
+    new_discussions = html_parser.get_new_discussions()
+    html_parser.save_discussions()
+    if new_discussions:
+        bot.send_message(chat_id=os.getenv("CHAT_ID"),
+                         text=str(new_discussions))
 
 
 run()
