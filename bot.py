a23c43bddf2e2fb2fe8e3e06d545b1e1c21d8717
Gleb Oborin <oborin.gleb@yandex.ru>
2019-05-30 01:44:11 +0300

    changed output

--- bot.py
+++ bot.py
@@ -3,24 +3,36 @@ import sys
 import telebot
 import html_parser
 
-bot = telebot.TeleBot(os.getenv("TOKEN"))
-SITE_URL = "обсуждения.екатеринбург.рф"
+bot = telebot.TeleBot(os.getenv('TOKEN'))
+SITE_URL = 'обсуждения.екатеринбург.рф'
 
-intro_new = """Добрый вечер.
+intro_new = '''
+[ ](обсуждения.екатеринбург.рф)
+Добрый вечер.
 Список новых обсуждений за сегодня:
-"""
+'''
 
-intro_all = """Добрый вечер.
+intro_all = '''
+[ ](обсуждения.екатеринбург.рф)
+Добрый вечер.
 Напоминаем, что в данный момент доступны следущие обсуждения:
-"""
+'''
+
+outro = '*Все текущие обсуждения*\nобсуждения.екатеринбург.рф'
+
+
+def format_disc(disc):
+    return '[' + disc[0] + '](' + SITE_URL + disc[1] + ')\n_' + disc[2] + '_'
 
 
 def send_discussions(getter, greeter):
     discussions = getter()
     if discussions:
         message_text = greeter + '\n' + \
-            '\n\n'.join(map('\n{}'.format(SITE_URL).join, discussions))
-        bot.send_message(chat_id=os.getenv("CHAT_ID"), text=message_text)
+            '\n\n'.join(map(format_disc, discussions)) + '\n\n' + outro
+        bot.send_message(chat_id=os.getenv('CHAT_ID'),
+                         text=message_text,
+                         parse_mode='Markdown')
 
 
 def send_all_discussions():
@@ -31,7 +43,7 @@ def send_new_discussions():
     send_discussions(html_parser.get_new_discussions, intro_new)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if sys.argv[1] == '--new':
         send_new_discussions()
     else:
